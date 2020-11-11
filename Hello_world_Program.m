3c9a637913295ed9e8682655bbb615dff4ee4775
schroederdo <73490338+schroederdo@users.noreply.github.com>
2020-11-11 11:53:43 -0700

    Add files via upload
    This is the first version of the program

---
+++ Hello_world_Program.m
@@ -0,0 +1,12 @@
+%% Test Program
+% Created 11 November, 2020
+% Author: Donavon Schroeder 
+
+%% We want this program to display "hello world" 
+
+%% hosuekeeping
+clear; close all; clc;
+
+% hello world 
+variable = 'Hello World';
+disp(variable)
